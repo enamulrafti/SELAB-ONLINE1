5ceeafd14e889ff4068c94dcc8c37b3f806e4862
enamulrafti <enamul.rafti@gmail.com>
2022-03-21 15:14:39 +0600

    leave application dash borad db config table added

--- leave_application_dashboard.php
+++ leave_application_dashboard.php
@@ -6,4 +6,24 @@
         $results = mysqli_query( $connect, "SELECT * FROM leave_enrty" )
 		or die("Can not execute query");
 
+        echo "<table border> \n";
+        echo "<th>Name</th> <th>Start</th> <th>End</th> <th>Status</th> \n";
+    
+        while( $rows = mysqli_fetch_array( $results ) ) {
+            extract( $rows );
+            echo "<tr>";
+            echo "<td> $EMPLOYEE_NAME </td>";
+            echo "<td> $LEAVE_START </td>";
+            echo "<td> $LEAVE_END </td>";
+            echo "<td> $STATUS1 </td>";
+            echo "<td> $STATUS1 </td>";
+            // echo "<td> <a href = 'delete.php?id=$id'> Delete </a> </td>";
+            // echo "<td> <a href = 'update_input.php?id=$id&f0=$f0&f1=$f1'> Update </a> </td>";
+            echo "</tr> \n";
+        }
+    
+        echo "</table> \n";
+    
+        echo "<p><a href=home.html> << Home </a>";
+
 ?>
