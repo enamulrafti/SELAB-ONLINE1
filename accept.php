ee094ca2ee83d11996a098d1cbe0926865eeeef5
enamulrafti <enamul.rafti@gmail.com>
2022-03-21 15:26:08 +0600

    accept.php DONE

--- accept.php
+++ accept.php
@@ -6,5 +6,24 @@
         $connect = mysqli_connect( HOST, USER, PASS, DB )
     
             or die("Can not connect");
+        
+        	$query 	= "UPDATE leave_enrty SET STATUS1='ACCPETED' WHERE ID = $accept_id";
+
+	        echo $query;
+
+
+
+	mysqli_query( $connect, $query )
+
+		or die("Can not execute query");
+
+
+
+	echo "<p>ACCEPTED!";
+
+
+
+	echo "<p><a href=home.html> << HOME </a>";
+
 
 ?>
