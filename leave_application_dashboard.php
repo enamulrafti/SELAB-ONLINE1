c41706b00d1cc5163a0c56c3e60fe0f0845669a3
enamulrafti <enamul.rafti@gmail.com>
2022-03-21 15:19:18 +0600

    Accept system added

--- leave_application_dashboard.php
+++ leave_application_dashboard.php
@@ -7,7 +7,7 @@
 		or die("Can not execute query");
 
         echo "<table border> \n";
-        echo "<th>Name</th> <th>Start</th> <th>End</th> <th>Status</th> <th>Remarks</th>  \n";
+        echo "<th>Name</th> <th>Start</th> <th>End</th> <th>Status</th> <th>Remarks</th> <th>Accept</th>  \n";
     
         while( $rows = mysqli_fetch_array( $results ) ) {
             extract( $rows );
@@ -17,7 +17,7 @@
             echo "<td> $LEAVE_END </td>";
             echo "<td> $STATUS1 </td>";
             echo "<td> $REMARKS </td>";
-            // echo "<td> <a href = 'delete.php?id=$id'> Delete </a> </td>";
+            echo "<td> <a href = 'accept.php?id=$ID'> Accept </a> </td>";
             // echo "<td> <a href = 'update_input.php?id=$id&f0=$f0&f1=$f1'> Update </a> </td>";
             echo "</tr> \n";
         }
